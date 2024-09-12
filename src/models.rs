21147d5c011e867208c790c93f6f1620e02e9619
Binghan Lin <binghan.lin@shonCloud.com>
2024-09-13 00:53:21 +0800

    start stage2 step1

--- src/models.rs
+++ src/models.rs
@@ -1,11 +1,25 @@
+use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 
-use serde::{Deserialize, Serialize};
+#[derive(Debug, PartialEq, Eq)]
+pub enum Action {
+    NavigateToEpicDetail { epic_id: u32 },
+    NavigateToStoryDetail { epic_id: u32, story_id: u32 },
+    NavigateToPreviousPage,
+    CreateEpic,
+    UpdateEpicStatus { epic_id: u32 },
+    DeleteEpic { epic_id: u32 },
+    CreateStory { epic_id: u32 },
+    UpdateStoryStatus { story_id: u32 },
+    DeleteStory { epic_id: u32, story_id: u32 },
+    Exit,
+}
 
 #[derive(Deserialize, Serialize, Debug, PartialEq, Clone)]
 pub enum Status {
     Open,
     InProgress,
+    Resolved,
     Closed,
 }
 
