431bfc8430e57b8fc331d5c2d5b359d26652e022
Binghan Lin <binghan.lin@shonCloud.com>
2024-09-17 16:08:58 +0800

    finish jira clone project.

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,57 @@
-mod db;
-mod io_utils;
+use std::rc::Rc;
+
 mod models;
-mod navigator;
+
+mod db;
+use db::*;
+
 mod ui;
 
+mod io_utils;
+use io_utils::*;
+
+mod navigator;
+use navigator::*;
+
 fn main() {
-    println!("Welcome To My-Jira!");
+    let db = Rc::new(JiraDatabase::new("./data/db.json".to_string()));
+    let mut navigator = Navigator::new(Rc::clone(&db));
+
+    loop {
+        clearscreen::clear().unwrap();
+
+        if let Some(page) = navigator.get_current_page() {
+            if let Err(e) = page.draw_page() {
+                print!("Error in drawing page: {}\nPress any key to continue...", e);
+
+                wait_for_key_press();
+            }
+
+            let user_input = get_user_input();
+
+            match page.handle_input(user_input.trim()) {
+                Err(e) => {
+                    print!(
+                        "Error in handling input: {}\nPress any key to continue...",
+                        e
+                    );
+
+                    wait_for_key_press();
+                }
+                Ok(action) => {
+                    if let Some(action) = action {
+                        if let Err(e) = navigator.handle_action(action) {
+                            print!(
+                                "Error in handling action: {}\nPress any key to continue...",
+                                e
+                            );
+                            wait_for_key_press();
+                        }
+                    }
+                }
+            }
+        } else {
+            break;
+        }
+    }
 }
